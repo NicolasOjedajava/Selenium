9fa58ec9156a1f677019bbd2331cc58ce7d9e88a
NicolasOjedajava <30484436+NicolasOjedajava@users.noreply.github.com>
2019-09-20 18:23:12 -0300

    Update XSSTest.java

--- SeleniumSecTest/src/test/java/OwaspTopTen/XSSTest.java
+++ SeleniumSecTest/src/test/java/OwaspTopTen/XSSTest.java
@@ -1,3 +1,5 @@
+package OwaspTopTen;
+ 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -16,6 +18,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
@@ -33,123 +36,49 @@ import org.openqa.selenium.UnhandledAlertException;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
         
-
-
 public class XSSTest {
     
     private WebDriver driver;
-    private Map<String, Object> vars;
-    JavascriptExecutor js;
-    private String pathDriver = "/home/deloitte/Descargas/geckodriver-v0.25.0-linux64/geckodriver";
-    
+    private String pathDriver = "C:\\chromedriver.exe";
     
     @Before
     public void setUp() {
-        
-        DesiredCapabilities dc = new DesiredCapabilities();
-        dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
-        System.setProperty("webdriver.gecko.driver",pathDriver);
-        driver = new FirefoxDriver(dc);
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
-        
-    }
-    
+        System.setProperty("webdriver.chrome.driver",pathDriver);
+        driver = new ChromeDriver();
+    }    
     
     @After
     public void tearDown() {
         driver.quit();
-    }
-    
+    }    
     
     @Test
     @SuppressWarnings("CallToPrintStackTrace")
-    public void xSS() {
-
-        ArrayList<String> payloads = cargarPayload();
-
-        for(String texto : payloads){
-            
-            driver.get("http://juice-shop-trainning.herokuapp.com/#/");
-            driver.manage().window().setSize(new Dimension(1920, 1040));
-            
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(XSSTest.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            WebElement searchBox = driver.findElement(By.id("searchQuery"));
-            WebElement searchButton = driver.findElement(By.id("searchButton"));
-            
-            WebDriverWait waitSeachBox = new WebDriverWait(driver,2000);
-            waitSeachBox.until(ExpectedConditions.elementToBeClickable(searchBox));
-            
-            searchBox.clear();
-            
-            searchBox.sendKeys(texto);
-            
-            try {
-                
-                WebDriverWait waitButton = new WebDriverWait(driver,2000);
-                waitButton.until(ExpectedConditions.elementToBeClickable(searchButton));
-                
-                searchButton.click();
-                
-                    try {
-                        Thread.sleep(4000);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(XSSTest.class.getName()).log(Level.SEVERE, null, ex);
-                    }   
-                
-            } catch (UnhandledAlertException f) {
-                
-                try {
-                        
-                    Alert alert = driver.switchTo().alert();
-                    String alertText = alert.getText();
-
-                    if(!(alertText.equals("")) && !(alertText.equals(null))){
-                        System.out.println("XXS INJECTION with payload: " + texto);
-                    }
-
-                    
-                        
-                } catch (NoAlertPresentException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }
-
-    }
-    
-    public ArrayList cargarPayload() {
-
-        //Cargar payload
-        String path = "/home/deloitte/XSS-Payload.txt"; //path txt payload.
-        String linea;
-        ArrayList <String> payloads = new ArrayList();
-
-        try {
-
-            FileReader fs = new FileReader(new File(path));
-            BufferedReader br = new BufferedReader(fs);
-
-            try {
-
-                while((linea=br.readLine())!=null){
-                    payloads.add(linea);
-                }
-
-            } catch (IOException ex) {
-                Logger.getLogger(XSSTest.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-        } catch (FileNotFoundException ex) {
-          Logger.getLogger(XSSTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return payloads;
-    }
+    public void xSS() {           
+            driver.get("https://juice-shop-trainning.herokuapp.com/#/");     
+ 
+            WebElement myDynamicElement = (new WebDriverWait(driver, 1000))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".close-dialog")));
+ 
+            WebElement btn = driver.findElement(By.cssSelector(".close-dialog"));
+            btn.click();  
+ 
+            (new WebDriverWait(driver, 1000)).until(ExpectedConditions.presenceOfElementLocated(By.id("searchQuery")));
+ 
+            WebElement textbox = driver.findElement(By.id("searchQuery"));
+            textbox.clear(); 
+            textbox.sendKeys("<iframe src=\"javascript:alert('XSS')\">");           
+ 
+            (new WebDriverWait(driver, 1000)).until(ExpectedConditions.presenceOfElementLocated(By.id("searchButton")));
+ 
+            WebElement btnBuscar = driver.findElement(By.id("searchButton"));
+            btnBuscar.click(); 
+ 
+            (new WebDriverWait(driver, 1000)).until(ExpectedConditions.alertIsPresent());
+ 
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();            
+                      
+            assertTrue("Vulnerable", alertText.equals("XXS"));                        
+        }    
 }
